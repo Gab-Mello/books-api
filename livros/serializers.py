259b12faea75af041644168871444e9e85b18012
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-05-23 23:27:43 -0300

    implementado nested relationship

--- livros/serializers.py
+++ livros/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Book, Author
 
-class BookSerializer(ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
 class AuthorSerializer(ModelSerializer):
     class Meta:
         model = Author
-        fields = '__all__'
+        fields = ('id','name','age','email')
+
+class BookSerializer(ModelSerializer):
+    author = AuthorSerializer()
+    class Meta:
+        model = Book
+        fields = ('id','author','title','description')
+
